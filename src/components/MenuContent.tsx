1ac02cc3459a2356423f91ec42c5658909a7e0aa
Victor Baptista <victorsgb@gmail.com>
2025-07-01 15:51:57 -0300

    Omitting pages of admin users only

--- src/components/MenuContent.tsx
+++ src/components/MenuContent.tsx
@@ -12,37 +12,29 @@ import DashboardIcon from '@mui/icons-material/Dashboard';
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
 import WorkspacesIcon from '@mui/icons-material/Workspaces';
 import SupportAgentIcon from '@mui/icons-material/SupportAgent';
-// import GroupsIcon from '@mui/icons-material/Groups';
 import ChatIcon from '@mui/icons-material/Chat';
 import { Badge, ListItemAvatar } from '@mui/material';
-// import ContactIcon from '@mui/icons-material/Contacts';
 
 import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
-// import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
-// import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
-// import HelpRoundedIcon from '@mui/icons-material/HelpRounded';
+import { useAuth } from '../context/AuthContext';
 
 const mainListItems = [
   { text: 'Dashboard', icon: <DashboardIcon /> },
-  { text: 'Plans', icon: <SubscriptionsIcon /> },
-  { text: 'Workspaces', icon: <WorkspacesIcon /> },
+  { text: 'Plans', icon: <SubscriptionsIcon />, role: 'ADMIN' },  // Add role condition here
+  { text: 'Workspaces', icon: <WorkspacesIcon />, role: 'ADMIN' }, // Add role condition here
   { text: 'Agents', icon: <SupportAgentIcon /> },
-  // { text: 'Team', icon: <GroupsIcon /> },
   { text: 'Chats', icon: <ChatIcon /> },
-  // { text: 'Contact', icon: <ContactIcon /> },
 ];
 
 const secondaryListItems = [
   { text: 'Billing', icon: <ReceiptLongIcon /> },
-  // { text: 'Settings', icon: <SettingsRoundedIcon /> },
-  // { text: 'About', icon: <InfoRoundedIcon /> },
-  // { text: 'Feedback', icon: <HelpRoundedIcon /> },
 ];
 
 export default function MenuContent() {
   const location = useLocation();
   const navigate = useNavigate();
 
+  const { user } = useAuth();
   const { paginatedAgents } = useAgents();
   const { agents } = paginatedAgents;
 
@@ -58,7 +50,6 @@ export default function MenuContent() {
     0: '/billing'
   };
 
-
   const totalUnreadCount = agents.reduce((total, wrapper) => {
     const chats = wrapper.agent?.paginatedChats?.data;
     if (!chats) return total;
@@ -73,36 +64,43 @@ export default function MenuContent() {
   return (
     <Stack sx={{ flexGrow: 1, p: 1, justifyContent: 'space-between' }}>
       <List dense>
-        {mainListItems.map((item, index) => (
-          <ListItem
-            key={index}
-            disablePadding
-            sx={{ display: 'block' }}
-            onClick={() => navigate(routePrimaryKeyMap[index])}
-          >
-            <ListItemButton selected={routePrimaryKeyMap[index] == location.pathname}>
-              {item.text == 'Chats' ? (
-                <>
-                  <ListItemIcon>{item.icon}</ListItemIcon>
-                  <ListItemAvatar>
-                    <Badge
-                      badgeContent={totalUnreadCount}
-                      color="error"
-                      invisible={totalUnreadCount === 0}
-                    >
-                      <ListItemText primary={item.text} />
-                    </Badge>
-                  </ListItemAvatar>
-                </>
-              ) : (
-                <>
-                  <ListItemIcon>{item.icon}</ListItemIcon>
-                  <ListItemText primary={item.text} />
-                </>
-              )}
-            </ListItemButton>
-          </ListItem>
-        ))}
+        {mainListItems.map((item, index) => {
+          // Only include "Plans" and "Workspaces" if the user's role is "ADMIN"
+          console.log({ user: user?.role.name, item: item.role})
+          if (item.role && user?.role.name !== item.role) {
+            return null;
+          }
+          return (
+            <ListItem
+              key={index}
+              disablePadding
+              sx={{ display: 'block' }}
+              onClick={() => navigate(routePrimaryKeyMap[index])}
+            >
+              <ListItemButton selected={routePrimaryKeyMap[index] == location.pathname}>
+                {item.text == 'Chats' ? (
+                  <>
+                    <ListItemIcon>{item.icon}</ListItemIcon>
+                    <ListItemAvatar>
+                      <Badge
+                        badgeContent={totalUnreadCount}
+                        color="error"
+                        invisible={totalUnreadCount === 0}
+                      >
+                        <ListItemText primary={item.text} />
+                      </Badge>
+                    </ListItemAvatar>
+                  </>
+                ) : (
+                  <>
+                    <ListItemIcon>{item.icon}</ListItemIcon>
+                    <ListItemText primary={item.text} />
+                  </>
+                )}
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
       </List>
       <List dense>
         {secondaryListItems.map((item, index) => (
